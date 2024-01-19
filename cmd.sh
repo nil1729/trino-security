8bfd667a1392d007aef42d1ff798b9671a91ad03
Nilanjan Deb <nilanjan172nsvian@gmail.com>
2024-01-19 20:44:55 +0530

    feature/nil-13 automate password.db creation

--- cmd.sh
+++ cmd.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+
+# create password for user (admin)
 htpasswd -B -C 10 password.db admin
+
--- playground/docker-entrypoint.sh
+++ playground/docker-entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 cp /usr/local/share/proxy/certs/nginx.crt /usr/local/share/ca-certificates && \
 update-ca-certificates && \
 cd /root/ && \
