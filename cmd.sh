9c43c0b7b24084c9a67c092394fca78af3d5eb6b
Nilanjan Deb <nilanjan.deb@inmobi.com>
2024-01-19 20:18:11 +0530

    feature/nil-13 initial commit

---
+++ cmd.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+htpasswd -B -C 10 password.db admin
---
+++ playground/docker-entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+cp /usr/local/share/proxy/certs/nginx.crt /usr/local/share/ca-certificates && \
+update-ca-certificates && \
+cd /root/ && \
+tail -f /dev/null
